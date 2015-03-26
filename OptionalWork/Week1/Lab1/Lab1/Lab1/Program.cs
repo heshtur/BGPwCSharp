8c4894b6c9fef9ad2ef803728c9044960ae045b5
heshtur <chrisga1990@gmail.com>
2015-03-25 17:25:38 -0700

    First two labs
    Labs completed doing stuff

---
+++ OptionalWork/Week1/Lab1/Lab1/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Christopher Patron");
+            Console.Write("\n");
+            Console.Write("Robert Half");
+            Console.Write("\n");
+            Console.Write("Borderlands, \n Half Life, \n and Neverwinter.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OptionalWork/Week1/Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 24;
+            Console.Write(age);
+            Console.ReadLine();
+            const int MAX_SCORE = 100;
+            int score = 36;
+            float percent = (float)score / (float)MAX_SCORE;
+            Console.Write(percent);
+            Console.ReadLine();
+
+        }
+    }
+}
