1f42e306dd47aba3e511769d4c6eb58e82d9a3e9
heshtur <chrisga1990@gmail.com>
2015-03-27 12:52:31 -0700

    Lab 3 completed

---
+++ OptionalWork/Week1/Lab3/Lab3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float originalFahrenheit;
+            Console.Write("Enter temperature (Fahrenheight): ");
+            originalFahrenheit = float.Parse(Console.ReadLine());
+
+            float FtoC = (originalFahrenheit - 32) / 9 * 5;
+            float CtoF = (float)((FtoC * 9) / 5) + 32;
+            
+            Console.WriteLine(originalFahrenheit + " degrees Fahrenheit is " + FtoC + " degrees Celsius.");
+            Console.WriteLine(FtoC + " degrees Celsius is " + CtoF + " degrees Fahrenheit");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
